85b0962184e04db88a633002f29b9123056e1cc2
jons <jonesazarya123@gmail.com>
2025-01-11 12:23:34 +0800

    feat: create project EduFun

---
+++ app/src/main/java/com/example/edufun/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.edufun
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.edufun.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val Intent = Intent(this, HomeActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+}
