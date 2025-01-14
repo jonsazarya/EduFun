aa35a3c3cee87f11431879eab9e653666749369c
jons <jonesazarya123@gmail.com>
2025-01-14 13:30:19 +0800

    fix: make Home Activity for EduFUn

--- app/src/main/java/com/example/edufun/HomeActivity.kt
+++ app/src/main/java/com/example/edufun/HomeActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.edufun.adapter.CategoryAdapter
+import com.example.edufun.databinding.ActivityHomeBinding
+import com.example.edufun.model.Category
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val categories = listOf(
+            Category("Matematika", "Belajar Matematika sangat menyenangkan!", R.drawable.math),
+            Category("IPA", "Belajar IPA sangat menyenangkan!", R.drawable.science),
+            Category("Seni Budaya", "Belajar Seni Budaya sangat menyenangkan!", R.drawable.seni_budaya)
+        )
+
+        val adapter = CategoryAdapter(categories)
+        binding.rvCategory.adapter = adapter
+        binding.rvCategory.layoutManager = LinearLayoutManager(this)
     }
 }
